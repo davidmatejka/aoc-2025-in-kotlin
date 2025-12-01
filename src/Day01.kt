7497c8d606d9659bc9d99030c1dfc528eb35dff9
david <david.b.matejka@gmail.com>
2025-12-01 06:18:20 +0100

    added solutions to part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,61 @@
 fun main() {
+    val dayNr = "01"
     fun part1(input: List<String>): Int {
-        return input.size
+        var dialPosition = 50
+        var sumLeftAtZero = 0
+
+        for (rotation in input) {
+            val dir = rotation.first()
+            val steps = rotation.drop(1).toInt()
+
+            when (dir) {
+                'R' -> dialPosition += steps
+                'L' -> dialPosition -= steps
+            }
+            dialPosition = dialPosition.mod(100)
+            if (dialPosition == 0) sumLeftAtZero++
+        }
+        return sumLeftAtZero
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var dialPosition = 50
+        var sumPointedAtZero = 0
+
+        for (rotation in input) {
+            val dir = rotation.first()
+            val steps = rotation.drop(1).toInt()
+
+            when (dir) {
+                'R' -> {
+                    (1..steps).forEach { _ ->
+                        dialPosition++
+                        dialPosition = dialPosition.mod(100)
+                        if (dialPosition == 0) {
+                            sumPointedAtZero++
+                        }
+                    }
+                }
+
+                'L' -> {
+                    (1..steps).forEach { _ ->
+                        dialPosition--
+                        dialPosition = dialPosition.mod(100)
+                        if (dialPosition == 0) {
+                            sumPointedAtZero++
+                        }
+                    }
+                }
+            }
+        }
+        return sumPointedAtZero
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val testInput = readInput("Day${dayNr}_test")
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day${dayNr}")
     part1(input).println()
     part2(input).println()
 }
